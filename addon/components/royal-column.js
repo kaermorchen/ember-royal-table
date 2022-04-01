38db8ec2eaab5880e9c9cd7254985ad5f700a038
Stanislav Romanov <kaermorchen@gmail.com>
2022-04-01 13:54:17 +0300

    Add sorting

--- addon/components/royal-column.js
+++ addon/components/royal-column.js
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class RoyalColumnComponent extends Component {
   get isHead() {
@@ -7,5 +8,39 @@ export default class RoyalColumnComponent extends Component {
 
   get isBody() {
     return this.args.place === 'body';
+  }
+
+  get isSorted() {
+    return this.args.sortingProp === this.args.prop;
+  }
+
+  @action
+  handleSort() {
+    if (typeof this.args.onSort !== 'function') {
+      return;
+    }
+
+    let key = null;
+
+    switch (this.args.sortingProp) {
+      case `${this.args.prop}:asc`:
+        key = `${this.args.prop}:desc`;
+        break;
+      case `${this.args.prop}:desc`:
+        key = null;
+        break;
+      default:
+        key = `${this.args.prop}:asc`;
+        break;
+    }
+
+    this.args.onSort(key);
+  }
+
+  @action
+  handleClickHead(e) {
+    if (typeof this.args.onClick === 'function') {
+      this.args.onClick(this, e);
+    }
   }
 }
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,6 +1,11 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { compare } from '@ember/utils';
+import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
+  @tracked sortingProp = 'firstName';
+
   data = [
     {
       firstName: 'Tony',
@@ -15,4 +20,46 @@ export default class ApplicationController extends Controller {
       age: 25,
     },
   ];
+
+  columns = [
+    {
+      name: 'First name',
+      prop: 'firstName',
+      order: 0,
+    },
+    {
+      name: 'Last name',
+      prop: 'lastName',
+      order: 1,
+    },
+    {
+      name: 'Age',
+      prop: 'age',
+      order: 2,
+    },
+  ];
+
+  get sortedData() {
+    if (!this.sortingProp) {
+      return this.data;
+    }
+
+    const sortingKeys = this.sortingProp.split(':');
+    const sortedData = this.data.sort((a, b) =>
+      compare(a[sortingKeys[0]], b[sortingKeys[0]])
+    );
+
+    return !sortingKeys[1] || sortingKeys[1] === 'asc'
+      ? sortedData
+      : sortedData.reverse();
+  }
+
+  get sortedColumns() {
+    return this.columns.sort((a, b) => b.order - a.order);
+  }
+
+  @action
+  onSort(prop) {
+    this.sortingProp = prop;
+  }
 }
