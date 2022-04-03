02767c3ad855bb3350f9c00e20f4fcfb45876574
Stanislav Romanov <kaermorchen@gmail.com>
2022-04-03 18:07:52 +0300

    Fixed head icons

--- addon/components/royal-table.js
+++ addon/components/royal-table.js
@@ -1,14 +1,3 @@
 import Component from '@glimmer/component';
-import { TrackedSet } from 'tracked-built-ins';
-import { htmlSafe } from '@ember/template';
 
-export default class RoyalTableComponent extends Component {
-  columns = new TrackedSet();
-
-  get style() {
-    const size = this.columns.size;
-    return size
-      ? htmlSafe(`grid-template-columns: repeat(${size}, 1fr);`)
-      : null;
-  }
-}
+export default class RoyalTableComponent extends Component {}
--- tests/dummy/app/components/royal-table/column/sorting-icon.js
+++ tests/dummy/app/components/royal-table/column/sorting-icon.js
@@ -1,3 +1,13 @@
 import Component from '@glimmer/component';
 
-export default class RoyalTableColumnSortingIconComponent extends Component {}
+export default class RoyalTableColumnSortingIconComponent extends Component {
+  get icon() {
+    if (this.args.isAscSorted) {
+      return 'chevron-down';
+    } else if (this.args.isDescSorted) {
+      return 'chevron-up';
+    }
+
+    return 'unfold-more-horizontal';
+  }
+}
